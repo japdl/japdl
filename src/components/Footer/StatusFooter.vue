70bb8bc4de248cd23089b574f488e2fe1c66fbac
Seysa <garion.g@hotmail.fr>
2021-09-15 21:24:34 +0200

    added footer (forgot about it

---
+++ src/components/Footer/StatusFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-400 w-full h-full hover:bg-blue-200 flex justify-center">
+    <h1 v-if="!downloads.length">No Downloads yet</h1>
+    <div v-else id="downloads">
+      <div v-for="(download, index) in downloads" :key="index">
+        {{ download }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const downloads = ref(["test"]);
+</script>

423750d5a33492de805c187748135d4eee9bfe8b
Seysa <garion.g@hotmail.fr>
2021-10-22 12:08:22 +0200

    better router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,42 +1,33 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Download from "../views/Download.vue";
-import Infos from "../views/Infos.vue";
-import Tests from "../views/Tests.vue";
-import Options from "../views/Options.vue";
+
+const makeRoute = (path: string, name?: string) => {
+  const upperCaseFirstLetter = (str: string): string => {
+    const splitted = str.split("");
+    splitted[0] = splitted[0].toUpperCase();
+    return splitted.join("");
+  };
+  const vueModuleName = `${upperCaseFirstLetter(path)}`;
+  const routeName = name ? name : vueModuleName;
+  return {
+    path: "/" + path,
+    name: routeName,
+    component: () => import(`../views/${vueModuleName}.vue`),
+  } as RouteRecordRaw;
+};
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Accueil",
     component: Home,
   },
-
-  {
-    path: "/download",
-    name: "Télécharger un manga",
-    component: Download,
-  },
-  {
-    path: "/infos",
-    name: "Informations sur un manga",
-    component: Infos,
-  },
-  {
-    path: "/about",
-    name: "À propos",
-    component: About,
-  },
-  {
-    path: "/tests",
-    name: "Tests",
-    component: Tests,
-  },
-  {
-    path: "/options",
-    name: "Options",
-    component: Options,
-  },
+  makeRoute("download", "Télécharger un manga"),
+  makeRoute("downloaded", "Mangas téléchargés"),
+  makeRoute("infos", "Informations sur un manga"),
+  makeRoute("about", "À propos"),
+  makeRoute("tests"),
+  makeRoute("options"),
 ];
 
 const router = createRouter({

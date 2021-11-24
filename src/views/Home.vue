47a803c88835a431745b86bfc9d0facdd658e68e
Seysa <garion.g@hotmail.fr>
2021-11-24 09:54:57 +0100

    centered 'changements' in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="text-xl text-center">
-    <h1 class="mb-4">Bienvenue sur japdl <b>{{ version }}</b>!</h1>
+    <h1 class="mb-4">
+      Bienvenue sur japdl <b>{{ version }}</b
+      >!
+    </h1>
     <div v-if="loading">Chargement...</div>
     <div v-else>
       <div v-if="state.needUpdate" class="flex flex-col justify-center">
-        <h2>La nouvelle version <b>{{ state.latestVersion }}</b> est disponible!</h2>
+        <h2>
+          La nouvelle version <b>{{ state.latestVersion }}</b> est disponible!
+        </h2>
         <div id="infos">
-          <p>La version <b>{{state.latestVersion}}</b> est sortie le {{ state.releaseDate }}.</p>
+          <p>
+            La version <b>{{ state.latestVersion }}</b> est sortie le
+            {{ state.releaseDate }}.
+          </p>
           <p>Changement(s):</p>
-          <div id="changelog" class="flex flex-col justify-left text-left">
-            <p id="line" v-for="line in state.changeLog.split('\r\n')" :key="line">{{ line }}</p>
+          <div class="flex justify-center items-center">
+            <div
+              id="changelog"
+              class="
+                flex flex-col
+                justify-center
+                text-left
+                w-2/3
+                border-4
+                p-4
+                border-dark-primary
+                rounded-xl
+              "
+            >
+              <p
+                id="line"
+                v-for="line in state.changeLog.split('\r\n')"
+                :key="line"
+              >
+                {{ line }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -56,7 +84,7 @@ const fetchLastRelease = async (): Promise<FetchResults> => {
     const latestRelease = response.data[0];
     const releaseDate = new Date(latestRelease.created_at).toLocaleDateString(
       "fr-FR",
-      { year: "numeric", month: "long", day: "numeric" }
+      { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" }
     );
     const changeLog = latestRelease.body;
     console.log("changelog", changeLog);

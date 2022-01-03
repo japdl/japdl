55d9869e77d42d941d4c29f6617c90969fbfd404
Seysa <garion.g@hotmail.fr>
2022-01-03 18:34:25 +0100

    added roboto font

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/tailwind.css";
 import "./assets/css/global.css";
+import "typeface-roboto/index.css";
 
 createApp(App).use(router).mount("#app");

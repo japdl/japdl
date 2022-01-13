436f15584445248827a38580828aeb3b0fbdc814
Seysa <garion.g@hotmail.fr>
2022-01-13 20:39:18 +0100

    fixed positioning

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,15 @@
 <template>
-  <nav class="select-none flex justify-evenly items-center rounded-b-2xl mb-1">
+  <nav
+    class="h-14 select-none flex justify-evenly items-center rounded-b-2xl bg-dark-background"
+  >
     <router-link
       v-for="route in routes"
       :key="route.name"
       :to="route.path"
       draggable="false"
-      >{{ route.name }}
+      class="h-full flex justify-center items-center mx-2 p-2 rounded-xl transition-all duration-200 w-full text-center min-w-max hover:text-primary"
+    >
+      <span>{{ route.name }}</span>
     </router-link>
   </nav>
 </template>
@@ -20,22 +24,10 @@ const routes = [
 </script>
 
 <style scoped>
-nav {
-  background-color: var(--dark-background);
-}
-
-nav a {
-  @apply mx-2 p-2 rounded-xl transition-all duration-200 w-full text-center min-w-max;
-}
 nav a.router-link-exact-active {
-  color: var(--dark-primary);
+  @apply text-dark-primary;
 }
 nav a:hover {
-  color: var(--primary);
-  background-image: linear-gradient(
-    to top,
-    var(--light-background),
-    var(--dark-background)
-  );
+  @apply text-primary bg-gradient-to-t from-light-background to-dark-background;
 }
 </style>

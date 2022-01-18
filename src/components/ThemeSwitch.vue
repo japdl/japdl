c9da948263b144f91fd05bf6905237ee919f079d
Seysa <garion.g@hotmail.fr>
2022-01-18 20:16:22 +0100

    fixed click zone

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="themeButton" class="flex justify-center items-center py-1">
+  <div
+    id="themeButton"
+    class="flex justify-center items-center py-1"
+    @click="switchTheme()"
+  >
     <Sun
       v-if="selectedTheme == 'dark'"
       ref="sun"
       src="../assets/svg/sun.svg"
       alt="sun"
-      class="full"
       @click="setTheme('light')"
     />
     <Moon
@@ -13,7 +16,6 @@
       ref="moon"
       src="../assets/svg/moon.svg"
       alt="moon"
-      class="full"
       @click="setTheme('dark')"
     />
   </div>
@@ -26,12 +28,20 @@ import Moon from "./Images/Moon.vue";
 import Sun from "./Images/Sun.vue";
 
 const selectedTheme = ref("dark");
+type Theme = "dark" | "light";
 
-const setTheme = (theme: "dark" | "light") =>
+const switchTheme = () => {
+  if (selectedTheme.value == "dark") {
+    selectedTheme.value = "light";
+  } else {
+    selectedTheme.value = "dark";
+  }
+  setTheme(selectedTheme.value as Theme);
+};
+
+const setTheme = (theme: Theme) =>
   ipcRenderer.send("setTheme", theme);
 
 ipcRenderer.send("getTheme");
 ipcRenderer.on("changeTheme", (event, arg) => (selectedTheme.value = arg));
 </script>
-
-<style scoped></style>

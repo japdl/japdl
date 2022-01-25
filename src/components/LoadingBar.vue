3ae22d3757b824717e2acae4fb44bc88bd3a67ca
Seysa <garion.g@hotmail.fr>
2022-01-25 11:36:50 +0100

    text sm fixes offset

--- src/components/LoadingBar.vue
+++ src/components/LoadingBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="loadingBar" class="w-full">
     <div id="fill" :style="widthStyle" class="flex items-center justify-center">
-      <span v-if="roundedPercent !== 0 && props.show"
+      <span class="text-sm" v-if="roundedPercent !== 0 && props.show"
         >{{ roundedPercent }}%</span
       >
     </div>

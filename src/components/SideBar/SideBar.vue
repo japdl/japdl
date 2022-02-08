9c06656bbfc11914032cd294c596845817cd2412
Seysa <garion.g@hotmail.fr>
2022-02-08 12:17:40 +0100

    simplified sidebar

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -2,15 +2,12 @@
   <aside
     class="flex flex-col bg-dark-background items-center w-72 h-full overflow-y-auto rounded"
   >
-    <h1 class="font-manga text-3xl my-2">Téléchargements</h1>
-    <div id="downloads" class="w-full flex flex-col gap-1 h-full">
+    <h1 class="font-manga text-2xl my-2">Téléchargements</h1>
+    <div id="downloads" class="w-full flex flex-col h-full">
       <h1 class="text-center opacity-70 pt-5" v-if="!displaysAnything">
         Aucun téléchargement en cours
       </h1>
-      <div
-        v-if="currentDownload"
-        class="border-b-2 border-t-2 text-center text-xl border-current"
-      >
+      <div v-if="currentDownload" class="text-center text-xl">
         <h2>En cours</h2>
         <SideDownload
           v-if="currentDownload"
@@ -22,20 +19,14 @@
         />
       </div>
 
-      <div
-        v-if="queue.length > 0"
-        class="border-b-2 border-t-2 text-center text-xl border-current"
-      >
+      <div v-if="queue.length > 0" class="text-center text-lg">
         <h2>En attente</h2>
         <div v-for="queueElement in queue" :key="queueElement">
           {{ queueElement }}
         </div>
       </div>
 
-      <div
-        v-if="done.length > 0"
-        class="border-b-2 border-t-2 text-center text-xl border-current"
-      >
+      <div v-if="done.length > 0" class="text-center text-lg">
         <h2>Terminés</h2>
 
         <div class="opacity-80" v-for="doneElement in done" :key="doneElement">
@@ -52,8 +43,8 @@ import { ipcRenderer } from "electron";
 import { computed, ref } from "vue";
 import SideDownload from "./SideDownload.vue";
 
-const queue = ref<string[]>([]);
-const done = ref<string[]>([]);
+const queue = ref<string[]>(["one-piece 999"]);
+const done = ref<string[]>(["one-piece 998"]);
 let currentDownload = ref<OngoingDownload | null>(null);
 const displaysAnything = computed(
   () =>
@@ -76,7 +67,7 @@ ipcRenderer.on("update-done", (event, data) => {
 
 <style scoped>
 h2 {
-  @apply text-center text-lg font-manga;
+  @apply text-center text-xl font-manga;
 }
 
 span,

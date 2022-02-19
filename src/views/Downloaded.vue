be3c1f06cd9aa359bd6643b5d1bc9636becfe6a3
Seysa <garion.g@hotmail.fr>
2022-02-19 16:24:35 +0100

    spaces are automatically replaced by dashes in search

--- src/views/Downloaded.vue
+++ src/views/Downloaded.vue
@@ -71,7 +71,7 @@ const iterableFolders = computed(() => {
     return folders.value.filter((folder) => {
       return path
         .basename(folder.path.toLowerCase())
-        .includes(search.value.toLowerCase());
+        .includes(search.value.toLowerCase().replaceAll(/ +/g, "-"));
     });
   }
 });

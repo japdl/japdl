3eec0d4d8087bb4eab2179559d04f8b2f09f1f4f
Seysa <garion.g@hotmail.fr>
2022-02-20 19:53:18 +0100

    loading can now handle 2 loading at once

--- src/utils/loadingState.ts
+++ src/utils/loadingState.ts
@@ -2,22 +2,32 @@ import { ref } from "vue";
 
 export const loading = ref(false);
 const loadingTime = ref<null | Date>(null);
+let stopTimeout: null | NodeJS.Timeout = null;
+
+let instancesLoading = 0;
 
 export function startLoading(): void {
+  if (stopTimeout) {
+    clearTimeout(stopTimeout);
+  }
   loading.value = true;
+  instancesLoading++;
   // if another component is already loading, then
   // it already started spinning, so we can't change
   // the date it started spinning
   if (loadingTime.value === null) {
     loadingTime.value = new Date();
   }
-  console.log("Starting loading at", loadingTime.value);
 }
 
 export function stopLoading(): void {
   if (!loadingTime.value) return;
   const msElapsed = new Date().getTime() - loadingTime.value.getTime();
-  console.log("Loading took " + msElapsed + "ms");
+  if (instancesLoading > 1) {
+    instancesLoading--;
+    return;
+  }
+  instancesLoading--;
   if (msElapsed <= 10) {
     loading.value = false;
     loadingTime.value = null;
@@ -25,10 +35,8 @@ export function stopLoading(): void {
   }
   // need to get ms left for next second
   const msLeft = 1000 - (msElapsed % 1000);
-  console.log("Waiting " + msLeft + "ms before stopping loading");
-  setTimeout(() => {
+  stopTimeout = setTimeout(() => {
     loading.value = false;
     loadingTime.value = null;
-    console.log("Loading stopped");
   }, msLeft);
 }

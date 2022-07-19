b0866336baa24e585d8b5d0e95cdfb71c0f7fb9f
Seysa <garion.g@hotmail.fr>
2022-07-20 00:45:54 +0200

    added some description

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="flex flex-col justify-center items-center gap-2">
+    <div id="explanatory" class="text-gray-300 mx-4">
+      <h1 class="text-2xl mb-2">Qu'est-ce que c'est ? Comment ça marche ?</h1>
+      <p>
+        Cet onglet sert à transférer les fichiers téléchargés de votre
+        ordinateur sur un autre appareil, par exemple votre téléphone.
+      </p>
+      <p class="step">
+        Vérifiez tout d'abord que votre ordinateur et l'autre appareil sont
+        connectés sur le même réseau (wifi, ethernet).
+      </p>
+      <p class="step">Activez le transfert grâce au bouton "Activer"</p>
+      <p class="step">
+        Ouvrez l'adresse indiquée dans le navigateur de votre appareil puis
+        choisissez le manga ainsi que le fichier à télécharger en cliquant
+        dessus
+      </p>
+    </div>
+
     <button
       @click="switchTransfer"
       class="p-2 rounded bg-opacity-70 text-center transition-colors"
@@ -45,3 +63,9 @@ function refreshNetwork() {
   network.value = getNetwork();
 }
 </script>
+
+<style scoped>
+.step::before {
+  content: "- ";
+}
+</style>
